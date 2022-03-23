f99695b373ffc19f883c21ee10c07a3275487947
Ludwing Cano <ludwing238@gmail.com>
2022-03-22 19:05:26 -0600

    adding stateComponent

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import StartApp from "./components/startApp.js"
+import TodoApp from "./components/stateComponents/todoApp.js"
 import Board from "./components/game.js"
 import "./index.css"
 ReactDOM.render(
-  <Board/>,
+  <TodoApp/>,
   document.getElementById("root")
 );
