449f482d5d9591f159eee33aaf6b17d813fc5363
Ludwing Cano <ludwing238@gmail.com>
2022-03-24 21:02:06 -0600

    Ejemplo Hooks

---
+++ src/components/stateComponents/todoAppHooks.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+
+export default function TodoAppHooks() {
+  // Declara una nueva variable de estado, que llamaremos "count".
+  const [tasks, setTasks] = useState([]);
+  const [text, setText] = useState("");
+
+  // Similar a componentDidMount y componentDidUpdate:
+  useEffect(() => {
+    // Actualiza el título del documento usando la Browser API
+    console.log("cambio");
+  });
+
+  function addElement($event) {
+    $event.preventDefault();
+    if (text.length == 0) {
+      return;
+    }
+
+    const newItem = {
+      text: text,
+      date: Date.now(),
+    };
+
+    let newTasks = tasks;
+    newTasks.push(newItem);
+    setText(text);
+    setTasks([...newTasks]);
+  }
+
+  function deleteElement(index) {
+    let updateTasks = tasks;
+    updateTasks.splice(index, 1);
+    console.log(index);
+    console.log(updateTasks);
+    setText("");
+    setTasks([...updateTasks]);
+  }
+  return (
+    <>
+      <h3>Add new Task: </h3>
+      <form>
+        <label>What do you need?</label>
+        <input
+          id="todo-input"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        ></input>
+        <button onClick={addElement}> Add new Task # {tasks.length + 1}</button>
+      </form>
+      <ul>
+        {tasks.map((element, index) => {
+          return (
+            <li
+              key={index}
+              onClick={() => {
+                deleteElement(index);
+              }}
+            >
+              {element.text}
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+}
